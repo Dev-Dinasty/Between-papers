b701574b6cac60ade3332922889f1c262bd6144b
Frederick <maussafrederick18@gmail.com>
2023-05-29 17:45:27 -0500

    Sales list added

--- between-papers/src/components/units-list.jsx
+++ between-papers/src/components/units-list.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+
 import '../stylesheets/book-unit.css';
 
-function BookUnit(props){
-  return(
+function UnitsList(props){
+   return(
     <div className="containerBookUnit">
       <div className="bookUnitImg">
         <img src={require(`../img/${props.img}.png`)} alt={props.alt}/>
@@ -12,11 +13,8 @@ function BookUnit(props){
         <p className="price">${props.price}</p>
       </div>
       <p>Stock: {props.stock}</p>
-      <div className="btnContainer">
-        <button>Agregar al carrito</button>
-      </div>
-    </div>
+    </div>    
   );
 }
 
-export default BookUnit;
+export default UnitsList;
